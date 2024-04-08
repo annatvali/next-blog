29efd8b321039dfb7069bc0255e5ef7a92738fe9
annatvali <anntvaliashvili@gmail.com>
2024-04-08 16:16:57 +0400

    feat: setup Prisma with PostgreSQL database and next-auth

---
+++ next-blog/app/home/Posts.tsx
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Props = {};
+
+const Posts = async (props: Props) => {
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+  const posts = await prisma.post.findMany();
+  console.log(posts);
+
+  return (
+    <>
+    <p>Posts</p>
+    </>
+  );
+}
+
+export default Posts;
