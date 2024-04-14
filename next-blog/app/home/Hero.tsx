dfd79fb6fe9c45c93b6517b8782e2582a2ca0622
annatvali <anntvaliashvili@gmail.com>
2024-04-14 22:30:50 +0400

    chore: replace  hero section image

--- next-blog/app/home/Hero.tsx
+++ next-blog/app/home/Hero.tsx
@@ -3,20 +3,20 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <section className='grid grid-cols-1 sm:grid-cols-12 gap-8 mt-8'>
-      <div className='w-full col-span-6 place-self-center text-center sm:text-left justify-self-start'>
-        <h1 className='text-3xl sm:text-4xl md:text-6xl md:leading-normal'>
+    <section className='grid grid-cols-1 xl:grid-cols-12 gap-8 mt-12'>
+      <div className='w-full col-span-4 place-self-center sm:text-left justify-self-start'>
+        <h1 className='text-2xl sm:text-3xl md:text-4xl md:leading-normal'>
           News for every urban toursit!
         </h1>
         <button className='text-xl text-white font-bold bg-green-400 boarder-2 rounded shadow-[0.3rem_0.3rem_0px_0px_rgba(0,0,0,0.85)] px-4 sm:px-6 py-2 sm:py-4 mt-4'>
           Explore more
         </button>
       </div>
-      <div className='col-span-6 justify-self-center sm:justify-self-end'>
+      <div className='col-span-8 justify-self-center sm:justify-self-end'>
         <Image
-          src='/kent-tupas-xVLdFIxcDCc-unsplash.jpg'
-          width={516.6}
-          height={344.416}
+          src='/tbilisi-cover.jpg'
+          width={1800}
+          height={1200}
           alt='People crossing pedestrian crossing'
         />
       </div>
