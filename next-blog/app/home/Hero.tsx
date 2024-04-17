b8145c623a679d943a8da9201fe0613e68b2d6ba
annatvali <anntvaliashvili@gmail.com>
2024-04-17 18:19:00 +0400

    refactor: move Subscription section from layout to home

--- next-blog/app/home/Hero.tsx
+++ next-blog/app/home/Hero.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
 type Props = {};
 
 const Hero = (props: Props) => {
@@ -8,9 +10,20 @@ const Hero = (props: Props) => {
         <h1 className='text-2xl sm:text-3xl md:text-4xl md:leading-normal'>
           News for every urban toursit!
         </h1>
-        <button className='text-xl text-white font-bold bg-green-400 boarder-2 rounded shadow-[0.3rem_0.3rem_0px_0px_rgba(0,0,0,0.85)] px-4 sm:px-6 py-2 sm:py-4 mt-4'>
-          Explore more
-        </button>
+        <div className='flex items-center gap-4 mt-8'>
+          <Link
+            href='/blog/all'
+            className='text-xl text-white font-bold bg-green-400 boarder-2 rounded shadow-[0.3rem_0.3rem_0px_0px_rgba(0,0,0,0.85)] px-4 sm:px-6 py-2 sm:py-4'
+          >
+            Explore blogs
+          </Link>
+          <Link
+            href='/blog/new'
+            className='text-xl text-white font-bold bg-blue-400 boarder-2 rounded shadow-[0.3rem_0.3rem_0px_0px_rgba(0,0,0,0.85)] px-4 sm:px-6 py-2 sm:py-4'
+          >
+            Write a blog ✍🏻
+          </Link>
+        </div>
       </div>
       <div className='col-span-8 justify-self-center sm:justify-self-end'>
         <Image
